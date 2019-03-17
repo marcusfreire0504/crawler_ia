d22d14848fa29d243effa5bab800a5e3ac71707f
leandrofreire <rafaelleandrofreire@gmail.com>
2019-03-16 21:47:17 -0300

    Scraping pegando as tabelas

--- crawler_ia/spiders/wiki_spider.py
+++ crawler_ia/spiders/wiki_spider.py
@@ -4,9 +4,7 @@ import pickle
 class WikiSpider(scrapy.Spider):
     name = 'wikispider'
     start_urls = [
-        'https://pt.wikipedia.org/wiki/Bilheteria_dos_cinemas_no_Brasil_em_2018',
-        'https://pt.wikipedia.org/wiki/Bilheteria_dos_cinemas_no_Brasil_em_2012'
-    ]
+        'https://pt.wikipedia.org/wiki/Bilheteria_dos_cinemas_no_Brasil_em_2018'    ]
 
     def parse(self, response):
         file = response.css('table')[1].getall()
